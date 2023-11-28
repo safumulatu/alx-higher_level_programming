8893172a4190eb069377feb444985ca066e0b365
safumulatu <safumulatu9@gmail.com>
2023-11-28 09:32:56 -0800

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
+for i in reversed(range(97, 123)):
+    if (i % 2 == 0):
+        print('{:c}'.format(i), end='')
     else:
-        print('{:c}'.format(90 - i), end='')
+        print('{:c}'.format(i - 32), end='')
