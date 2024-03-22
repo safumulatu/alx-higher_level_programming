b27a0a342ff22375eaf6720057a8ef72e2dde5a4
safumulatu <safumulatu9@gmail.com>
2024-03-22 10:39:35 -0700

    whats that

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""Lists states"""
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    new_state = State(name="California")
-    session.add(new_state)
-    session.commit()
-    new_city = City(name="San Francisco", state_id=new_state.id)
-    session.add(new_city)
-    session.commit()
-    session.close()
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+class City(Base):
+    """Class representing the states table"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False, primary_key=True,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
